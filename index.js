9b8c4a955439cc5978218c06642e3d8f2b9897b1
JD Pravin <dogfly6@yahoo.com>
2024-07-02 16:45:53 -0500

    exercise 3.6

--- index.js
+++ index.js
@@ -56,6 +56,21 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+
+    if (body.name === "" || body.number === "" ) {
+        response.status(400).json({
+            error: 'name or number is not provided'
+        })
+    }
+
+    let existingPerson = persons.find((person) => person.name === body.name)
+
+    if(existingPerson) {
+        response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const person = {
         name: body.name,
         number: body.number,
