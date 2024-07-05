4c4e8b9decfb8c070f56b949172cfb7bdd45ec3f
JD Pravin <dogfly6@yahoo.com>
2024-07-04 22:28:28 -0500

    exercise 3.12

---
+++ phone_book_backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = encodeURIComponent(process.argv[2]);
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://dogfly6:${password}@cluster0.aqt97ld.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (name && number) {
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else if (name || number) {
+  console.log('missing name or number')
+} else {
+  console.log('phonebook')
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
