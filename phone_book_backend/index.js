c3a641f27f93a8048d7843c6f5cf50ce9e4795e8
JD Pravin <dogfly6@yahoo.com>
2024-07-12 11:29:29 -0500

    exercise 3.15

--- phone_book_backend/index.js
+++ phone_book_backend/index.js
@@ -88,10 +88,11 @@ app.post('/api/persons', (request, response) => {
 
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
 })
 
 const PORT = 3001
